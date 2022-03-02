44624cf9cd1968a634761e86b195f4f70004cc78
Saliou1920 <diopsaliou45@gmail.com>
2022-03-02 14:53:21 -0400

    :rotating_light: testing paging

--- core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/JDBCExecutor.java
+++ core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/JDBCExecutor.java
@@ -1,9 +1,8 @@
 package ca.jrvs.apps.jdbc;
 
 import java.sql.Connection;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.List;
 
 public class JDBCExecutor {
 
@@ -14,24 +13,13 @@ public class JDBCExecutor {
         try {
             Connection connection = dcm.getConnection();
             CustomerDAO customerDao = new CustomerDAO(connection);
-            Customer customer = new Customer();
-            customer.setFirstName("Momo");
-            customer.setLastName("Fall");
-            customer.setEmail("mom.fall@abc.com");
-            customer.setPhone("123-456-7890");
-            customer.setAddress("232 Main St");
-            customer.setCity("Toronto");
-            customer.setState("ON");
-            customer.setZipCode("M5A2B3");
+            customerDao.findAllSorted(20).forEach(System.out::println);
+            System.out.println("\n");
+            System.out.println("Page");
+            for (int i = 1; i < 3; i++) {
+                customerDao.findAllPaged(10, i).forEach(System.out::println);
+            }
 
-            customerDao.create(customer);
-            System.out.println("Customer created: " + customer);
-
-            customerDao.findById(customer.getId());
-            System.out.println("Customer found: " + customer);
-            // delete customer
-            customerDao.delete(customer.getId());
-            System.out.println("Customer deleted: " + customer);
         } catch (SQLException e) {
             e.printStackTrace();
         }
