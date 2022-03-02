f4db2e936abb7bbf3381f7ab9140300d99383323
Saliou1920 <diopsaliou45@gmail.com>
2022-03-02 14:54:53 -0400

    :rocket: learning transaction

--- core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/CustomerDAO.java
+++ core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/CustomerDAO.java
@@ -4,6 +4,7 @@ import ca.jrvs.apps.jdbc.util.DataAccessObject;
 
 import javax.xml.crypto.Data;
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CustomerDAO extends DataAccessObject<Customer> {
@@ -15,6 +16,10 @@ public class CustomerDAO extends DataAccessObject<Customer> {
     private static final String DELETE_SQL = "DELETE FROM customer WHERE customer_id = ?";
     private static final String GET_ONE = "SELECT customer_id, first_name, last_name, email, phone, address, city, " +
             "state, zipcode FROM customer WHERE customer_id = ?";
+    private static final String GET_ALL_LMT = "SELECT customer_id, first_name, last_name, email, phone, address, " +
+            "city, state, zipcode FROM customer ORDER BY last_name, first_name LIMIT ?";
+    private static final String GET_ALL_PAGED = "SELECT customer_id, first_name, last_name, email, phone, address, " +
+            "city, state, zipcode FROM customer ORDER BY last_name, first_name LIMIT ? OFFSET ?";
 
     public CustomerDAO(Connection connection) {
         super(connection);
@@ -53,6 +58,12 @@ public class CustomerDAO extends DataAccessObject<Customer> {
     @Override
     public Customer update(Customer dto) {
         Customer customer = null;
+        try{
+            this.connection.setAutoCommit(false);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
         try(PreparedStatement statement = connection.prepareStatement(UPDATE_SQL)) {
             statement.setString(1, dto.getFirstName());
             statement.setString(2, dto.getLastName());
@@ -64,8 +75,15 @@ public class CustomerDAO extends DataAccessObject<Customer> {
             statement.setString(8, dto.getZipCode());
             statement.setLong(9, dto.getId());
             statement.executeUpdate();
+            this.connection.commit();
             customer = findById(dto.getId());
         } catch (SQLException e) {
+            try {
+                this.connection.rollback();
+            } catch (SQLException e1) {
+                e1.printStackTrace();
+                throw new RuntimeException(e1);
+            }
             e.printStackTrace();
             throw new RuntimeException(e);
         }
@@ -91,7 +109,58 @@ public class CustomerDAO extends DataAccessObject<Customer> {
             throw new RuntimeException(e);
         }
     }
+    public List<Customer> findAllSorted(int limit) {
+        List<Customer> customers = new ArrayList<>();
+        try(PreparedStatement statement = connection.prepareStatement(GET_ALL_LMT)) {
+            statement.setInt(1, limit);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                Customer customer = new Customer();
+                customer.setId(resultSet.getLong("customer_id"));
+                customer.setFirstName(resultSet.getString("first_name"));
+                customer.setLastName(resultSet.getString("last_name"));
+                customer.setEmail(resultSet.getString("email"));
+                customer.setPhone(resultSet.getString("phone"));
+                customer.setAddress(resultSet.getString("address"));
+                customer.setCity(resultSet.getString("city"));
+                customer.setState(resultSet.getString("state"));
+                customer.setZipCode(resultSet.getString("zipcode"));
+                customers.add(customer);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+        return customers;
+    }
 
+    public List<Customer> findAllPaged(int limit, int pageNumber) {
+        List<Customer> customers = new ArrayList<>();
+        int offset = (pageNumber - 1) * limit;
+        try(PreparedStatement statement = connection.prepareStatement(GET_ALL_PAGED);) {
+            if (limit < 1) limit = 10;
+            statement.setInt(1, limit);
+            statement.setInt(2, offset);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                Customer customer = new Customer();
+                customer.setId(resultSet.getLong("customer_id"));
+                customer.setFirstName(resultSet.getString("first_name"));
+                customer.setLastName(resultSet.getString("last_name"));
+                customer.setEmail(resultSet.getString("email"));
+                customer.setPhone(resultSet.getString("phone"));
+                customer.setAddress(resultSet.getString("address"));
+                customer.setCity(resultSet.getString("city"));
+                customer.setState(resultSet.getString("state"));
+                customer.setZipCode(resultSet.getString("zipcode"));
+                customers.add(customer);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+        return customers;
+    }
     @Override
     public void delete(long id) {
         try(PreparedStatement statement = connection.prepareStatement(DELETE_SQL)) {
--- core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/OrderDAO.java
+++ core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/OrderDAO.java
@@ -34,12 +34,13 @@ public class OrderDAO extends DataAccessObject<Order> {
                     order.setCustomerLastName(resultSet.getString(2));
                     order.setCustomerEmail(resultSet.getString(3));
                     order.setId(resultSet.getLong(4));
-                    order.setCreationDate(new Date (resultSet.getTimestamp(5).getTime()));
+                    orderId = order.getId();
+                    order.setCreationDate(new Date(resultSet.getDate(5).getTime()));
                     order.setTotalDue(resultSet.getBigDecimal(6));
                     order.setStatus(resultSet.getString(7));
                     order.setSalespersonFirstName(resultSet.getString(8));
                     order.setSalespersonLastName(resultSet.getString(9));
-                    order.setSalespersonEmail(resultSet.getString(10));
+                    order.setSalespersonEmail(resultSet.getString(10));;
                 }
                 OrderLine orderLine = new OrderLine();
                 orderLine.setQuantity(resultSet.getInt(11));
@@ -75,5 +76,49 @@ public class OrderDAO extends DataAccessObject<Order> {
     @Override
     public void delete(long id) {
 
+    }
+
+    public List<Order> getOrdersForCustomer(long customerId){
+        List<Order> orders = new ArrayList<>();
+        try(PreparedStatement statement = connection.prepareStatement(GET_FOR_CUST);){
+            statement.setLong(1, customerId);
+            ResultSet resultSet = statement.executeQuery();
+            long orderId = 0;
+            Order order = null;
+            while(resultSet.next()){
+                long localOrderId = resultSet.getLong(4);
+                if(orderId!=localOrderId){
+                    order = new Order();
+                    orders.add(order);
+                    order.setId(localOrderId);
+                    orderId = localOrderId;
+                    order.setCustomerFirstName(resultSet.getString(1));
+                    order.setCustomerLastName(resultSet.getString(2));
+                    order.setCustomerEmail(resultSet.getString(3));
+                    order.setCreationDate(new Date(resultSet.getDate(5).getTime()));
+                    order.setTotalDue(resultSet.getBigDecimal(6));
+                    order.setStatus(resultSet.getString(7));
+                    order.setSalespersonFirstName(resultSet.getString(8));
+                    order.setSalespersonLastName(resultSet.getString(9));
+                    order.setSalespersonEmail(resultSet.getString(10));
+                    List<OrderLine> orderLines = new ArrayList<>();
+                    order.setOrderLines(orderLines);
+                }
+                OrderLine orderLine = new OrderLine();
+                orderLine.setQuantity(resultSet.getInt(11));
+                orderLine.setProductCode(resultSet.getString(12));
+                orderLine.setProductName(resultSet.getString(13));
+                orderLine.setProductSize(resultSet.getInt(14));
+                orderLine.setProductVariety(resultSet.getString(15));
+                orderLine.setProductPrice(resultSet.getBigDecimal(16));
+                if (order != null) {
+                    order.getOrderLines().add(orderLine);
+                }
+            }
+        }catch(SQLException e){
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+        return orders;
     }
 }
