72eb13a798cab5f597487fc884154cd02933eddc
Saliou1920 <diopsaliou45@gmail.com>
2022-03-18 15:35:52 -0300

    :tada: test env

--- core_java/twitter/src/main/java/ca/jrvs/apps/twitter/Main.java
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/Main.java
@@ -6,6 +6,9 @@ public class Main {
     public static void main(String[] args) {
         Dotenv dotenv = Dotenv.load();
         System.out.println(dotenv.get("API_KEY"));
+        System.out.println(dotenv.get("API_KEY_SECRET"));
+        System.out.println(dotenv.get("ACCESS_TOKEN")) ;
+        System.out.println(dotenv.get("ACCESS_TOKEN_SECRET"));
     }
 
 }
