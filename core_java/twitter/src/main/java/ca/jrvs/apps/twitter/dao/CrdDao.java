f5f5832735c135b793b450fccf24652b387ce264
Saliou1920 <diopsaliou45@gmail.com>
2022-03-25 16:06:15 -0300

    :tada: implementing Twitter Dao

--- core_java/twitter/src/main/java/ca/jrvs/apps/twitter/dao/CrdDao.java
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/dao/CrdDao.java
@@ -1,4 +1,24 @@
 package ca.jrvs.apps.twitter.dao;
 
-public interface CrdDao {
+public interface CrdDao <T, ID> {
+    /**
+     * Create a new entity
+     * @param entity the entity to create
+     * @return the created entity
+     */
+    T create(T entity);
+
+    /**
+     * Find an entity by id
+     * @param id the id of the entity to find
+     * @return the found entity
+     */
+    T findById(ID id);
+
+    /**
+     * Delete an entity by id
+     * @param entity the entity to delete
+     * @return the deleted entity
+     */
+    T delete(T entity);
 }
---
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/dao/TwitterDao.java
@@ -0,0 +1,19 @@
+package ca.jrvs.apps.twitter.dao;
+
+import ca.jrvs.apps.twitter.model.Tweet;
+
+public class TwitterDao implements CrdDao<Tweet, String> {
+  @Override
+  public Tweet create(Tweet tweet) {
+    return null;
+  }
+  @Override
+  public Tweet delete(Tweet tweet) {
+    return null;
+  }
+
+  @Override
+  public Tweet findById(String id) {
+    return null;
+  }
+}
