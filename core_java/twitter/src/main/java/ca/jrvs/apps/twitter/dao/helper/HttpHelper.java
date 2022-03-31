cce08af60827d3b8057ce7760fc64f5159228200
Saliou1920 <diopsaliou45@gmail.com>
2022-03-31 13:00:47 -0300

    :bulb: add documentation

--- core_java/twitter/src/main/java/ca/jrvs/apps/twitter/dao/helper/HttpHelper.java
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/dao/helper/HttpHelper.java
@@ -1,21 +1,22 @@
 package ca.jrvs.apps.twitter.dao.helper;
 
+import java.io.IOException;
 import  java.net.URI;
-import java.net.http.HttpResponse;
+import org.apache.http.HttpResponse;
 
 public interface HttpHelper {
 
     /**
      * Execute a HTTP Post call
-     * @param uri
+     * @param uri URI to call
      * @return response body
      */
-    HttpResponse httpPost(URI uri);
+    HttpResponse httpPost(URI uri) throws IOException;
 
     /**
      * Execute a HTTP Get call
-     * @param uri
+     * @param uri URI to call
      * @return response body
      */
-    HttpResponse httpGet(URI uri);
+    HttpResponse httpGet(URI uri) throws IOException;
 }
