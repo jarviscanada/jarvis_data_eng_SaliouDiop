73a17c95b88178da0d1207be1f3388b8188183c0
Saliou1920 <diopsaliou45@gmail.com>
2022-04-03 15:51:07 -0300

    :tada: implement service

--- core_java/twitter/src/main/java/ca/jrvs/apps/twitter/service/Service.java
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/service/Service.java
@@ -1,2 +1,37 @@
-package ca.jrvs.apps.twitter.service;public class Service {
+package ca.jrvs.apps.twitter.service;
+
+import ca.jrvs.apps.twitter.model.Tweet;
+
+import java.io.IOException;
+import java.util.List;
+
+public interface Service {
+
+    /**
+     * Post a tweet
+     *
+     * @param tweet the tweet to post
+     * @return the posted tweet
+     */
+    Tweet postTweet(Tweet tweet) throws IOException;
+
+    /**
+     * Show a tweet
+     *
+     * @param id the id of the tweet to show
+     * @return the tweet
+     * @throws IllegalArgumentException if the tweet does not exist
+     */
+
+    Tweet showTweet(String id, String[] fields);
+
+    /*
+     * Delete a tweet
+     * @param id the id of the tweet to delete
+     * @return A list of deleted tweets
+     * @throws IllegalArgumentException if the tweet does not exist
+     */
+
+    List<Tweet> deleteTweet(String[] id);
+
 }
