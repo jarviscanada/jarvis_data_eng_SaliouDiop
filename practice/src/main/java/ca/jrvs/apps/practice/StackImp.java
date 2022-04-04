b49882749d5a2a0ce3b6d83aa02b07648bc5bcfc
Saliou1920 <diopsaliou45@gmail.com>
2022-04-04 12:56:57 -0300

    :tada: Stack implementation using queue

--- practice/src/main/java/ca/jrvs/apps/practice/StackImp.java
+++ practice/src/main/java/ca/jrvs/apps/practice/StackImp.java
@@ -1,2 +1,43 @@
-package ca.jrvs.apps.practice;public class StackImp {
+package ca.jrvs.apps.practice;
+
+import java.util.LinkedList;
+
+public class StackImp {
+
+    private final LinkedList<Integer> queue;
+
+    public StackImp() {
+        queue = new LinkedList<>();
+    }
+
+    public void push(int data) {
+        queue.addFirst(data);
+    }
+
+    public int pop() {
+        return queue.removeFirst();
+    }
+
+    public int peek() {
+        return queue.getFirst();
+    }
+
+    public boolean isEmpty() {
+        return queue.isEmpty();
+    }
+
+    public int size() {
+        return queue.size();
+    }
+
+    public static void main(String[] args) {
+        StackImp stack = new StackImp();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.isEmpty());
+    }
 }
