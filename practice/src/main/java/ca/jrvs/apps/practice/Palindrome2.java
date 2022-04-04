21e5b1d78a2d2904bb40d488a6bd550e1fb48a74
Saliou1920 <diopsaliou45@gmail.com>
2022-04-04 17:24:38 -0300

    :tada: implement Valid Palindrome II

--- practice/src/main/java/ca/jrvs/apps/practice/Palindrome2.java
+++ practice/src/main/java/ca/jrvs/apps/practice/Palindrome2.java
@@ -1,2 +1,35 @@
-package ca.jrvs.apps.practice;public class Palindrome2 {
+package ca.jrvs.apps.practice;
+
+public class Palindrome2 {
+    // Given a string s, return true if the s can be palindrome after deleting at most one character from it.
+
+
+    // Example 1:
+    // Input: s = "abca"
+    // Output: true
+    // Explanation: We can delete the character 'c' from s to get a palindrome.
+
+    public boolean validPalindrome(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return isPalindrome(s, i + 1, j) || isPalindrome(s, i, j - 1);
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    private boolean isPalindrome(String s, int i, int j) {
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
 }
