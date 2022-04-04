a8368678d2028e8ea76266ced79f9b885ad3e518
Saliou1920 <diopsaliou45@gmail.com>
2022-04-04 17:25:13 -0300

    :sparkles: implement valaid parentheses

--- practice/src/main/java/ca/jrvs/apps/practice/ValidParentheses.java
+++ practice/src/main/java/ca/jrvs/apps/practice/ValidParentheses.java
@@ -1,2 +1,37 @@
-package ca.jrvs.apps.practice;public class ValidParentheses {
+package ca.jrvs.apps.practice;
+
+import java.util.Stack;
+
+public class ValidParentheses {
+
+    public boolean isValid(String s) {
+        if (s == null || s.length() == 0) {
+            return true;
+        }
+        if (s.length() % 2 != 0) {
+            return false;
+        }
+        char[] chars = s.toCharArray();
+        Stack<Character> stack = new Stack<>();
+        for (char c : chars) {
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char top = stack.pop();
+                if (top == '(' && c != ')') {
+                    return false;
+                }
+                if (top == '[' && c != ']') {
+                    return false;
+                }
+                if (top == '{' && c != '}') {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
 }
