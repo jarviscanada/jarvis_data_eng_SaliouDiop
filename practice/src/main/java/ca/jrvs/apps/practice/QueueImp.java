8be15a48d114f27b1b840ebff73d241421b1020d
Saliou1920 <diopsaliou45@gmail.com>
2022-04-04 12:57:18 -0300

    :tada: Queue implementation using stack

--- practice/src/main/java/ca/jrvs/apps/practice/QueueImp.java
+++ practice/src/main/java/ca/jrvs/apps/practice/QueueImp.java
@@ -1,2 +1,43 @@
-package ca.jrvs.apps.practice;public class QueueImp {
+package ca.jrvs.apps.practice;
+
+import java.util.Stack;
+
+public class QueueImp {
+    private final Stack<Integer> stack1;
+    private final Stack<Integer> stack2;
+
+    public QueueImp() {
+        stack1 = new Stack<>();
+        stack2 = new Stack<>();
+    }
+
+    public void enqueue(int item) {
+        stack1.push(item);
+    }
+
+    public int dequeue() {
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.pop();
+    }
+
+    public int peek() {
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.peek();
+    }
+
+    public boolean isEmpty() {
+        return stack1.isEmpty() && stack2.isEmpty();
+    }
+
+    public int size() {
+        return stack1.size() + stack2.size();
+    }
 }
