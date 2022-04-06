3f14ee4ed2a653b631279bfce53231dcc080d77c
Saliou1920 <diopsaliou45@gmail.com>
2022-04-06 13:22:21 -0300

    :tada: rotate  string problem

--- practice/src/main/java/ca/jrvs/apps/practice/RotatingString.java
+++ practice/src/main/java/ca/jrvs/apps/practice/RotatingString.java
@@ -1,2 +1,30 @@
-package ca.jrvs.apps.practice;public class RotatingString {
+package ca.jrvs.apps.practice;
+
+public class RotatingString {
+    public boolean rotateString(String s, String goal) {
+        // if s can become goal after some number of rotations, return true
+        // else return false
+
+        if (s.length() != goal.length()) {
+            return false;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            String temp = s.substring(i) + s.substring(0, i);
+            System.out.println(temp);
+            if (temp.equals(goal)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        RotatingString rs = new RotatingString();
+        System.out.println(rs.rotateString("abcde", "cdeab"));
+
+        String s = "abcde";
+        String goal = "cdeab";
+        System.out.println(s.substring(1) + s.charAt(0));
+    }
 }
