49363b6e839c0cdb60b65e3e559e63db7cbcf0c5
Saliou1920 <diopsaliou45@gmail.com>
2022-04-06 13:22:49 -0300

    :tada: valid anagram problem

--- practice/src/main/java/ca/jrvs/apps/practice/ValidAnagram.java
+++ practice/src/main/java/ca/jrvs/apps/practice/ValidAnagram.java
@@ -1,2 +1,23 @@
-package ca.jrvs.apps.practice;public class ValidAnagram {
+package ca.jrvs.apps.practice;
+
+public class ValidAnagram {
+
+    public boolean isValidAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int[] map = new int[26];
+        for(int i : s.toCharArray()){
+            map[i - 'a']++;
+        }
+        for(int i : t.toCharArray()){
+            map[i - 'a']--;
+        }
+        for(int i : map){
+            if(i != 0){
+                return false;
+            }
+        }
+        return true;
+    }
 }
