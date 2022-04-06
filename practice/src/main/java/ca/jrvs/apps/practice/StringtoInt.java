47a9781b92d1488d39badbbd1f7e240de5c59308
Saliou1920 <diopsaliou45@gmail.com>
2022-04-06 13:23:19 -0300

    :tada string to int problem

--- practice/src/main/java/ca/jrvs/apps/practice/StringtoInt.java
+++ practice/src/main/java/ca/jrvs/apps/practice/StringtoInt.java
@@ -1,2 +1,27 @@
-package ca.jrvs.apps.practice;public class StringtoInt {
+package ca.jrvs.apps.practice;
+
+public class StringtoInt {
+    public static int myAtoi(String str) {
+        if (str == null || str.length() == 0) {
+            return 0;
+        }
+        str = str.trim();
+        int i = 0;
+        int sign = 1;
+        if (str.charAt(0) == '+') {
+            i++;
+        } else if (str.charAt(0) == '-') {
+            sign = -1;
+            i++;
+        }
+        long result = 0;
+        while (i < str.length() && str.charAt(i) >= '0' && str.charAt(i) <= '9') {
+            result = result * 10 + (str.charAt(i) - '0');
+            if (result > Integer.MAX_VALUE) {
+                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            }
+            i++;
+        }
+        return (int) (sign * result);
+    }
 }
