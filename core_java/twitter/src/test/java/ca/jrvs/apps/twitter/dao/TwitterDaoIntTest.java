63fc8eb3971904daf3b988c06e386db77e2a67b4
Saliou1920 <diopsaliou45@gmail.com>
2022-04-07 15:07:58 -0300

    :tada: controller interface

---
+++ core_java/twitter/src/test/java/ca/jrvs/apps/twitter/dao/TwitterDaoIntTest.java
@@ -0,0 +1,20 @@
+package ca.jrvs.apps.twitter.dao;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TwitterDaoIntTest {
+
+    @Test
+    public void create() {
+    }
+
+    @Test
+    public void findById() {
+    }
+
+    @Test
+    public void delete() {
+    }
+}
