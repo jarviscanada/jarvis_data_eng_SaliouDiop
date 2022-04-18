0c40ccf9c49ecb2a32f997e097c8886101d15798
Saliou1920 <diopsaliou45@gmail.com>
2022-04-18 11:24:15 -0300

    :tada: implement only digits problem

--- practice/src/main/java/ca/jrvs/apps/practice/OnlyDigits.java
+++ practice/src/main/java/ca/jrvs/apps/practice/OnlyDigits.java
@@ -1,2 +1,83 @@
-package ca.jrvs.apps.practice;public class OnlyDigits {
+package ca.jrvs.apps.practice;
+
+public class OnlyDigits {
+
+    // check if a string contains only digits
+
+    public static boolean isDigit(String s) {
+        if (s == null || s.isEmpty()) {
+            return false;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (!Character.isDigit(s.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // check if a string contains only digits
+
+    public static boolean isDigit2(String s) {
+        if (s == null || s.isEmpty()) {
+            return false;
+        }
+        return s.matches("[0-9]+");
+    }
+
+    // check if a string contains only digits
+
+    public static boolean isDigit3(String s) {
+        if (s == null || s.isEmpty()) {
+            return false;
+        }
+        return s.chars().allMatch(Character::isDigit);
+    }
+
+    // check if a string contains only digits
+
+    public static boolean isDigit4(String s) {
+        if (s == null || s.isEmpty()) {
+            return false;
+        }
+        return s.chars().allMatch(Character::isDigit);
+    }
+
+    // check if a string contains only digits
+
+    public static boolean isDigit5(String s) {
+        if (s == null || s.isEmpty()) {
+            return false;
+        }
+        return s.chars().allMatch(Character::isDigit);
+    }
+
+    // check if a string contains only digits
+
+    public static boolean isDigit6(String s) {
+        if (s == null || s.isEmpty()) {
+            return false;
+        }
+        return s.chars().allMatch(Character::isDigit);
+    }
+
+    // check if a string contains only digits
+
+    public static boolean isDigit7(String s) {
+        if (s == null || s.isEmpty()) {
+            return false;
+        }
+        return s.chars().allMatch(Character::isDigit);
+    }
+
+    // check if a string contains only digits
+
+    public static boolean isDigit8(String s) {
+        if (s == null || s.isEmpty()) {
+            return false;
+        }
+        return s.chars().allMatch(Character::isDigit);
+    }
+
+    // check if a string contains only digits
 }
