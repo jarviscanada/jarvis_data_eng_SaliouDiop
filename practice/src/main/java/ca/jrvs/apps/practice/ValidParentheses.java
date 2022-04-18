a3a11597d5977fb00d39e482af3df41d97c90816
Saliou1920 <diopsaliou45@gmail.com>
2022-04-18 11:25:18 -0300

    :tada: implement valid parentheses

--- practice/src/main/java/ca/jrvs/apps/practice/ValidParentheses.java
+++ practice/src/main/java/ca/jrvs/apps/practice/ValidParentheses.java
@@ -5,33 +5,32 @@ import java.util.Stack;
 public class ValidParentheses {
 
     public boolean isValid(String s) {
-        if (s == null || s.length() == 0) {
-            return true;
-        }
-        if (s.length() % 2 != 0) {
-            return false;
-        }
-        char[] chars = s.toCharArray();
+        if(s.length() == 0) return false;
+
+        if(s.length() % 2 != 0 ) return false;
+
+        char[] characters = s.toCharArray();
         Stack<Character> stack = new Stack<>();
-        for (char c : chars) {
-            if (c == '(' || c == '[' || c == '{') {
+
+        for(char c : characters) {
+
+            if(c == '(' || c == '{' || c == '[') {
                 stack.push(c);
             } else {
-                if (stack.isEmpty()) {
-                    return false;
-                }
+
+                if(stack.isEmpty()) return false;
+
+
                 char top = stack.pop();
-                if (top == '(' && c != ')') {
-                    return false;
-                }
-                if (top == '[' && c != ']') {
-                    return false;
-                }
-                if (top == '{' && c != '}') {
-                    return false;
-                }
+                if(top ==  '(' && c != ')') return false;
+
+                if(top ==  '{' && c != '}') return false;
+
+                if(top ==  '[' && c != ']') return false;
+
             }
         }
+
         return stack.isEmpty();
     }
 }
