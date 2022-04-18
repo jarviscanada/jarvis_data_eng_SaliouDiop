67e61729b3bcffa2d0104abe60314db9c03f3295
Saliou1920 <diopsaliou45@gmail.com>
2022-04-18 11:24:44 -0300

    :tada: implement remove Nth Node

--- practice/src/main/java/ca/jrvs/apps/practice/RemoveNthNode.java
+++ practice/src/main/java/ca/jrvs/apps/practice/RemoveNthNode.java
@@ -1,2 +1,29 @@
-package ca.jrvs.apps.practice;public class RemoveNthNode {
+package ca.jrvs.apps.practice;
+
+public class RemoveNthNode {
+
+    static class node {
+        int data;
+        node next;
+    }
+
+    public static node removeNthNodeFromEnd(node head, int n) {
+        node slow = head;
+        node fast = head;
+        int count = 0;
+        while (count < n) {
+            fast = fast.next;
+            count++;
+        }
+        if (fast == null) {
+            head = head.next;
+            return head;
+        }
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+        return head;
+    }
 }
