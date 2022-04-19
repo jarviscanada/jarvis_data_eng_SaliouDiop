1b0e5d284c6b986cbfe5c17c9cc8e291fb9fc62f
Saliou1920 <diopsaliou45@gmail.com>
2022-04-19 16:50:36 -0300

    :tada: initial commit

---
+++ springboot/src/test/java/ca/jrvs/apps/tradingapp/TradingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.jrvs.apps.tradingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
