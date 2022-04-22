1510071b6860aa687e1ae83eac35a7b77b355522
Saliou1920 <diopsaliou45@gmail.com>
2022-04-22 12:20:26 -0300

    :hammer: implement Quote Service

--- springboot/src/main/java/ca/jrvs/apps/tradingapp/service/QuoteService.java
+++ springboot/src/main/java/ca/jrvs/apps/tradingapp/service/QuoteService.java
@@ -1,4 +1,13 @@
 package ca.jrvs.apps.tradingapp.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@Service
 public class QuoteService {
+
+    private static final Logger logger = LoggerFactory.getLogger(QuoteService.class);
 }
