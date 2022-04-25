48a6f427f0c312e3e4c66b9a75a75f84e246414f
Saliou1920 <diopsaliou45@gmail.com>
2022-04-25 13:06:11 -0300

    :hammer: test configuration

---
+++ springboot/src/test/java/ca/jrvs/apps/tradingapp/TestConfig.java
@@ -0,0 +1,47 @@
+package ca.jrvs.apps.tradingapp;
+
+import ca.jrvs.apps.tradingapp.model.config.MarketDataConfig;
+import io.github.cdimascio.dotenv.Dotenv;
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.apache.http.conn.HttpClientConnectionManager;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+@ComponentScan(basePackages = {"ca.jrvs.apps.tradingapp.dao", "ca.jrvs.apps.tradingapp.service"})
+public class TestConfig {
+
+    @Bean
+    public MarketDataConfig marketDataConfig() {
+        MarketDataConfig marketDataConfig = new MarketDataConfig();
+        marketDataConfig.setHost("https://cloud.iexapis.com/v1/");
+        marketDataConfig.setToken(Dotenv.load().get("IEX_TOKEN"));
+        return marketDataConfig;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        System.out.println("Creating test datasource");
+        String url = "jdbc:postgresql://localhost:5432/tradingapp_test";
+        String user = "postgres";
+        String password = "postgres";
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setUrl(url);
+        dataSource.setUsername(user);
+        dataSource.setPassword(password);
+        return dataSource;
+    }
+
+    @Bean
+    public HttpClientConnectionManager httpClientConnectionManager() {
+        PoolingHttpClientConnectionManager  cm = new PoolingHttpClientConnectionManager();
+        cm.setMaxTotal(100);
+        cm.setDefaultMaxPerRoute(100);
+        return cm;
+    }
+
+}
