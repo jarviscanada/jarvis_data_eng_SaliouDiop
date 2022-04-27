8b79dd1a0663607a23106894abf5600feda3b70d
Saliou1920 <diopsaliou45@gmail.com>
2022-04-27 14:24:38 -0300

    :hammer: app configuration

--- springboot/src/main/java/ca/jrvs/apps/tradingapp/TradingAppApplication.java
+++ springboot/src/main/java/ca/jrvs/apps/tradingapp/TradingAppApplication.java
@@ -1,13 +1,27 @@
 package ca.jrvs.apps.tradingapp;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
-public class TradingAppApplication {
+@SpringBootApplication(exclude = {
+		DataSourceAutoConfiguration.class,
+		HibernateJpaAutoConfiguration.class,
+		JdbcTemplateAutoConfiguration.class})
+public class TradingAppApplication implements CommandLineRunner {
+	private Logger logger = LoggerFactory.getLogger(TradingAppApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(TradingAppApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info("Application started");
+	}
 }
