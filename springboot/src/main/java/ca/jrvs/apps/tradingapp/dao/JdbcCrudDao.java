f9e7728867dcb2ef71335837516ebfd3ef238161
Saliou1920 <diopsaliou45@gmail.com>
2022-05-01 13:50:36 -0300

    :sparkles: trader dao

--- springboot/src/main/java/ca/jrvs/apps/tradingapp/dao/JdbcCrudDao.java
+++ springboot/src/main/java/ca/jrvs/apps/tradingapp/dao/JdbcCrudDao.java
@@ -1,2 +1,80 @@
-package ca.jrvs.apps.tradingapp.dao;public class JdbcCrudDao {
+package ca.jrvs.apps.tradingapp.dao;
+
+import ca.jrvs.apps.tradingapp.model.domain.Entity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+
+import java.util.Optional;
+
+public abstract class JdbcCrudDao<T extends Entity<Integer>> implements CrudRepository<T, Integer> {
+
+    private static final Logger logger = LoggerFactory.getLogger(JdbcCrudDao.class);
+    abstract public JdbcTemplate getJdbcTemplate();
+    abstract public SimpleJdbcInsert getSimpleJdbcInsert();
+    abstract public String getTableName();
+    abstract public String getIdColumnName();
+    abstract public Class<T> getEntityClass();
+
+    @Override
+    public <S extends T> S save(S entity) {
+        return null;
+    }
+
+    @Override
+    public <S extends T> Iterable<S> saveAll(Iterable<S> entities) {
+        return null;
+    }
+
+    @Override
+    public Optional<T> findById(Integer integer) {
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean existsById(Integer integer) {
+        return false;
+    }
+
+    @Override
+    public Iterable<T> findAll() {
+        return null;
+    }
+
+    @Override
+    public Iterable<T> findAllById(Iterable<Integer> integers) {
+        return null;
+    }
+
+    @Override
+    public long count() {
+        return 0;
+    }
+
+    @Override
+    public void deleteById(Integer integer) {
+
+    }
+
+    @Override
+    public void delete(T entity) {
+
+    }
+
+    @Override
+    public void deleteAllById(Iterable<? extends Integer> integers) {
+
+    }
+
+    @Override
+    public void deleteAll(Iterable<? extends T> entities) {
+
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
 }
--- springboot/src/main/java/ca/jrvs/apps/tradingapp/dao/TraderDao.java
+++ springboot/src/main/java/ca/jrvs/apps/tradingapp/dao/TraderDao.java
@@ -1,2 +1,48 @@
-package ca.jrvs.apps.tradingapp.dao;public class TraderDao {
+package ca.jrvs.apps.tradingapp.dao;
+
+import ca.jrvs.apps.tradingapp.model.domain.Trader;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+
+import javax.sql.DataSource;
+
+public class TraderDao extends JdbcCrudDao<Trader> {
+    private final String TABLE_NAME = "trader";
+    private final String COLUMN_ID = "id";
+
+    private JdbcTemplate jdbcTemplate;
+    private SimpleJdbcInsert jdbcInsert;
+
+    public TraderDao(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+        this.jdbcInsert = new SimpleJdbcInsert(dataSource)
+                .withTableName(TABLE_NAME)
+                .usingGeneratedKeyColumns(COLUMN_ID);
+    }
+
+    @Override
+    public JdbcTemplate getJdbcTemplate() {
+        return jdbcTemplate;
+    }
+
+    @Override
+    public SimpleJdbcInsert getSimpleJdbcInsert() {
+        return jdbcInsert;
+    }
+
+    @Override
+    public String getTableName() {
+        return TABLE_NAME;
+    }
+
+    @Override
+    public String getIdColumnName() {
+        return COLUMN_ID;
+    }
+
+    @Override
+    public Class<Trader> getEntityClass() {
+        return Trader.class;
+    }
+
 }
