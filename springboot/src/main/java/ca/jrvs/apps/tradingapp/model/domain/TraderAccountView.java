58f1796bc6043ae23a2f7f3181de67c406d6edf2
Saliou1920 <diopsaliou45@gmail.com>
2022-05-03 14:38:33 -0300

    :tada: implement trader account view  model

--- springboot/src/main/java/ca/jrvs/apps/tradingapp/model/domain/TraderAccountView.java
+++ springboot/src/main/java/ca/jrvs/apps/tradingapp/model/domain/TraderAccountView.java
@@ -1,2 +1,28 @@
-package ca.jrvs.apps.tradingapp.model.domain;public class TraderAccountView {
+package ca.jrvs.apps.tradingapp.model.domain;
+
+public class TraderAccountView {
+
+    private Account account;
+    private Trader trader;
+
+    public TraderAccountView(Trader trader, Account account) {
+        this.trader = trader;
+        this.account = account;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public Trader getTrader() {
+        return trader;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+
+    public void setTrader(Trader trader) {
+        this.trader = trader;
+    }
 }
