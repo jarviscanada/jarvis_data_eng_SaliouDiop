a583e844f145de918a8c5c3ae4ca7f1ec2b7e48e
Saliou1920 <diopsaliou45@gmail.com>
2022-05-03 14:37:14 -0300

    :tada: implement position dao

--- springboot/src/main/java/ca/jrvs/apps/tradingapp/dao/PostionDao.java
+++ springboot/src/main/java/ca/jrvs/apps/tradingapp/dao/PostionDao.java
@@ -1,2 +1,41 @@
-package ca.jrvs.apps.tradingapp.dao;public class PostionDao {
+package ca.jrvs.apps.tradingapp.dao;
+
+import ca.jrvs.apps.tradingapp.model.domain.Position;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+
+import javax.sql.DataSource;
+import java.util.List;
+
+public class PostionDao {
+
+    private final String TABLE_NAME = "position";
+    private final String COLUMN_ID = "id";
+
+    private JdbcTemplate jdbcTemplate;
+    private SimpleJdbcInsert jdbcInsert;
+
+    @Autowired
+    public PostionDao(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+        this.jdbcInsert = new SimpleJdbcInsert(dataSource)
+                .withTableName(TABLE_NAME)
+                .usingGeneratedKeyColumns(COLUMN_ID);
+    }
+
+    public List<Position> findAll() {
+        String sql = "SELECT * FROM " + TABLE_NAME;
+        return jdbcTemplate.query(sql, BeanPropertyRowMapper.newInstance(Position.class));
+    }
+
+    public Position findById(Integer id) {
+        String sql = "SELECT * FROM " + TABLE_NAME + " WHERE " + COLUMN_ID + " = ?";
+        return jdbcTemplate.queryForObject(sql, BeanPropertyRowMapper.newInstance(Position.class), id);
+    }
+
+    public long count() {
+        return findAll().size();
+    }
 }
